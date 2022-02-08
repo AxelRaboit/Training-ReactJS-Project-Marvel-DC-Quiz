c3d4eca87dc4a846d52b9b5d9401e3e9336b7fdf
Axel Raboit <axel.raboit@gmail.com>
2022-02-08 12:14:51 +0100

    config firebase

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,33 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyDIXuJLirZM-HLX1OylTg0RXSw310E9qw8",
+    authDomain: "marvel-dc-quiz-579ee.firebaseapp.com",
+    projectId: "marvel-dc-quiz-579ee",
+    storageBucket: "marvel-dc-quiz-579ee.appspot.com",
+    messagingSenderId: "446349184951",
+    appId: "1:446349184951:web:af92312f021d6e22bd6232"
+};
+
+class Firebase {
+    constructor() {
+        app.initializeApp(config);
+        this.app.auth();
+    }
+
+    signupUser = (email, password) => {
+     this.auth.createUserWithEmailAndPassword(email, password);
+    }
+
+    loginUser = (email, password) => {
+        this.auth.signInWithEmailAndPassword(email, password);
+    }
+
+    signoutUser = () => {
+        this.auth.signOut();
+    }
+
+}
+
+export default Firebase;
