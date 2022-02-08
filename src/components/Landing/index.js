d7fc8f8f7c7e6f0b6261211f292d1302decd9329
Axel Raboit <axel.raboit@gmail.com>
2022-02-08 11:41:29 +0100

    step to create routes and style the error page

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect, useState, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
 
@@ -33,11 +34,11 @@ const Landing = () => {
     const displayBtn = btn && (
         <Fragment>
             <div onMouseOver={setLeftImg} onMouseOut={clearImg} className='leftBox'>
-                <button className='btn-welcome'>Inscription</button>
+                <Link to="signup" className='btn-welcome'>Inscription</Link>
             </div>
 
             <div onMouseOver={setRightImg} onMouseOut={clearImg} className='rightBox'>
-                <button className='btn-welcome'>Connexion</button>
+                <Link to="login" className='btn-welcome'>Connexion</Link>
             </div>
         </Fragment>
     )
