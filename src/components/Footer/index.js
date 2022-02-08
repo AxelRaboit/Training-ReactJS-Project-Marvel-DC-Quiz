8220119610412d9e5ff3c1698e7f838fb5cee9e9
Axel Raboit <axel.raboit@gmail.com>
2022-02-08 10:58:18 +0100

    init the project + begin the landing page

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className='footer-container'>
+                <p>Projet réalisé à titre d'exercice</p>
+                <p>Les icônes proviennent de iconFinder.com</p>
+            </div>
+        </footer>
+    )
+};
+
+export default Footer;
