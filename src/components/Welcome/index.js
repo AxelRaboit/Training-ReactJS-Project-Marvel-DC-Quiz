14b5a5d4e7641d6dc8b1c1427937aac0dd973340
Axel Raboit <axel.raboit@gmail.com>
2022-02-09 11:34:59 +0100

    manager the user session with firebase

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState, useContext, Fragment, useEffect } from 'react';
 import Logout from '../Logout';
 import Quiz from '../Quiz';
+import { FirebaseContext } from '../Firebase';
 
-const Welcome = () => {
-    return (
+const Welcome = (props) => {
+
+    const firebase = useContext(FirebaseContext);
+
+    const [userSession, setUserSession] = useState(null);
+
+    useEffect(() => {
+        let listener = firebase.auth.onAuthStateChanged(user => {
+            user
+                ? setUserSession(user)
+                : props.history.push('/')
+        })
+
+        return () => {
+            //Permet de cleaner l'effet
+            listener()
+        }
+    },[])
+
+    return userSession === null ? (
+        <Fragment>
+            <div className="loader">
+                <p>Loading ...</p>
+            </div>
+        </Fragment>
+    ) : (
         <div className='quiz-bg'>
-            <div className='container'>
+            <div className="container">
                 <Logout />
                 <Quiz />
             </div>
