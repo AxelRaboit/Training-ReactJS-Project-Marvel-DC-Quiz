bb9dbf824ca6965bbbb2424651fe6ab5cf580cb1
Axel Raboit <axel.raboit@gmail.com>
2022-02-09 14:00:54 +0100

    init the quiz without dynamic data

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import Levels from '../Levels';
+import ProgressBar from '../ProgressBar';
 
 class Quiz extends Component {
-    
+
     render() {
 
-        const { pseudo } = this.props.userData; 
+        /* const { email, pseudo } = this.props.userData; */
         
         return (
             <div>
-                <h2>Pseudo: {pseudo}</h2>
-            </div>        
+                <Levels />
+                <ProgressBar/>
+                <h2>Notre question Quiz</h2>
+                <p className="answerOptions">Question 1</p>
+                <p className="answerOptions">Question 1</p>
+                <p className="answerOptions">Question 1</p>
+                <p className="answerOptions">Question 1</p>
+                <button className="btnSubmit">Suivant</button>
+            </div>
         )
     }
 };
