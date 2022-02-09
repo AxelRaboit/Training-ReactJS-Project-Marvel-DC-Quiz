63858400ac8b4f59361e112333200485405f8d3f
Axel Raboit <axel.raboit@gmail.com>
2022-02-09 17:19:01 +0100

    validate button and progressbar component

--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -1,18 +1,26 @@
 import React, { Fragment } from 'react'
 
-const ProgressBar = () => {
+const ProgressBar = ({ idQuestion, maxQuestions }) => {
+
+    const getPercentage = (totalQuestions, questionId) => {
+        return (100 / totalQuestions) * questionId;
+    }
+
+    const currentQuestion = idQuestion + 1;
+    const progressPercentage = getPercentage(maxQuestions, currentQuestion);
+
     return (
         <Fragment>
-        <div className='percentage'>
-            <div className='progressPercent'>Question: 1/10</div>
-            <div className='progressPercer'>Progression: 10%</div>
-        </div>
-        <div className='progressBar'>
-            <div className='progressBarChange' style={{width: '10%'}}></div>
-        </div>
+            <div className='percentage'>
+                <div className='progressPercent'>{`Question: ${currentQuestion}/${maxQuestions}`}</div>
+                <div className='progressPercer'>{`Progression: ${progressPercentage}%`}</div>
+            </div>
+            <div className='progressBar'>
+                <div className='progressBarChange' style={{width: `${progressPercentage}%`}}></div>
+            </div>
         </Fragment>
         
     )
 }
 
-export default ProgressBar
+export default React.memo(ProgressBar)
