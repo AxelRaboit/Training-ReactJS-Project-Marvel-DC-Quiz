2367c785fdeea512db1a956c678e6a813a37186e
Axel Raboit <axel.raboit@gmail.com>
2022-02-10 14:20:45 +0100

    install react tool tip and use it to the logout button

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
+import ReactTooltip from 'react-tooltip';
 import { FirebaseContext } from '../Firebase';
 
 const Logout = () => {
@@ -26,8 +27,12 @@ const Logout = () => {
                     type="checkbox"
                     checked={checked}
                 />
-                <span className="slider round"></span>
+                <span className="slider round" data-tip="Déconnexion"></span>
             </label>
+            <ReactTooltip
+                place="left"
+                effect="solid"
+            />
         </div>
     )
 };
